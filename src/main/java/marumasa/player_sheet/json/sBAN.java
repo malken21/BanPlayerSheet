d7bb33d617e7d93e32c1c88ea979d1ec9d733dfe
malken21 <Malken_Marumasa@outlook.jp>
2023-06-05 17:42:42 +0900

    update

---
+++ src/main/java/marumasa/player_sheet/json/sBAN.java
@@ -0,0 +1,11 @@
+package marumasa.player_sheet.json;
+
+public class sBAN {
+    public String type;
+    public boolean result;
+
+    public sBAN(String type, boolean result) {
+        this.type = type;
+        this.result = result;
+    }
+}
---
+++ src/main/java/marumasa/player_sheet/json/unsBAN.java
@@ -0,0 +1,11 @@
+package marumasa.player_sheet.json;
+
+public class unsBAN {
+    public String type;
+    public boolean result;
+
+    public unsBAN(String type, boolean result) {
+        this.type = type;
+        this.result = result;
+    }
+}
